269d6184cd0d8ff14693023a982251e75e4ff78c
Arushad Ahmed <dash-8x@hotmail.com>
2024-10-13 17:44:24 +0500

    - Added option to disable checkbox on table row
    Signed-off-by: Arushad Ahmed <dash-8x@hotmail.com>

--- resources/views/bootstrap-5/table/row.blade.php
+++ resources/views/bootstrap-5/table/row.blade.php
@@ -2,7 +2,7 @@
     @if(empty($noCheckbox))
         <td>
             <div class="form-check">
-                <input id="{{ $getCheckboxId() }}" data-check="{{ $name }}" name="{{ $name }}[]" value="{{ $modelId }}" class="form-check-input" type="checkbox" @if($disableCheckbox)disabled@endif />
+                <input id="{{ $getCheckboxId() }}" data-check="{{ $name }}" name="{{ $name }}[]" value="{{ $modelId }}" class="form-check-input" type="checkbox" @if($disableCheckbox)disabled @endif />
                 <label for="{{ $getCheckboxId() }}" class="form-check-label"></label>
             </div>
         </td>
--- resources/views/material-admin-26/table/row.blade.php
+++ resources/views/material-admin-26/table/row.blade.php
@@ -2,7 +2,7 @@
     @if(empty($noCheckbox))
         <td class="td-checkbox">
             <div class="checkbox">
-                <input id="{{ $getCheckboxId() }}" data-check="{{ $name }}" name="{{ $name }}[]" value="{{ $modelId }}" type="checkbox" @if($disableCheckbox)disabled@endif />
+                <input id="{{ $getCheckboxId() }}" data-check="{{ $name }}" name="{{ $name }}[]" value="{{ $modelId }}" type="checkbox" @if($disableCheckbox)disabled @endif />
                 <label  class="checkbox__label" for="{{ $getCheckboxId }}"></label>
             </div>
         </td>
