78bd2f04e293bb830ebb0d6671a73e45f83905da
athphane <athphane@gmail.com>
2025-02-28 00:25:46 +0500

    feat: ability to edit the amounts array

--- src/Views/Components/PerPage.php
+++ src/Views/Components/PerPage.php
@@ -6,14 +6,19 @@ class PerPage extends Component
 {
     protected string $view = 'per-page';
 
+    public array $amounts;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
     public function __construct(
+        array  $amounts = [10 => 10, 20 => 20, 50 => 50, 100 => 100, 500 => 500],
         string $framework = '',
-    ) {
+    )
+    {
+        $this->amounts = $amounts;
         parent::__construct($framework);
     }
 
