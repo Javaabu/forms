0908901002d886d1a58dabdf304af2dc1bd233a2
Arushad Ahmed <dash-8x@hotmail.com>
2025-04-16 18:03:46 +0500

    - Fixed search form placeholder
    Signed-off-by: Arushad Ahmed <dash-8x@hotmail.com>

--- resources/views/bootstrap-5/search-form.blade.php
+++ resources/views/bootstrap-5/search-form.blade.php
@@ -1,7 +1,7 @@
 <x-forms::form :action="route($route, $params)" class="search" :method="$method" :framework="$framework" :files="$files">
     <x-forms::text
         :name="$name"
-        :placeholder="$placeholder ?: trans('forms::search_form_placeholder')"
+        :placeholder="$placeholder ?: trans('forms::strings.search_form_placeholder')"
         :show-label="false"
         :framework="$framework"
     />
