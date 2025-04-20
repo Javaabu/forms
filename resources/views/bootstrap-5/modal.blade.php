dfde89c71cc603d9b3a2e4152aa61ad96b7ed867
athphane <athphane@gmail.com>
2025-04-20 15:48:32 +0500

    feat: better slot handling feat: material-26 modal

--- resources/views/bootstrap-5/modal.blade.php
+++ resources/views/bootstrap-5/modal.blade.php
@@ -1,10 +1,15 @@
-<div class="modal" tabindex="-1" id="{{ $attributes->get('id') ?: $id() }}">
+<div
+    {{ $attributes->merge(['class' => 'modal fade', 'tabindex' => -1]) }}
+    id="{{ $attributes->get('id') ?: $id() }}"
+>
     <div class="modal-dialog {{ $modalSizeClass ?? 'modal-lg' }}">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">
-                    {{ $title ?? '' }}
-                </h5>
+                @if($title)
+                    <h5 class="modal-title">
+                        {{ $title }}
+                    </h5>
+                @endif
 
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
@@ -13,9 +18,11 @@
                 {{ $slot }}
             </div>
 
-            <div class="modal-footer">
-                {{ $footer ?? '' }}
-            </div>
+            @if($footer)
+                <div class="modal-footer">
+                    {{ $footer }}
+                </div>
+            @endif
         </div>
     </div>
 </div>
