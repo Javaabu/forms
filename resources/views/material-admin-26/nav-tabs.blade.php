eeccd975b8d4c3cd81a619cf114011b7103b4a4b
Hussain Afeef <ibnahnajjaar@gmail.com>
2025-05-07 14:01:47 +0500

    feat: add icon support to nav tabs

--- resources/views/material-admin-26/nav-tabs.blade.php
+++ resources/views/material-admin-26/nav-tabs.blade.php
@@ -9,6 +9,10 @@
             ])
             href="{{ $item['url'] ?? '#' }}"
             >
+                @if(isset($item['icon']))
+                    <i class="{{ $item['icon'] }} mr-2"></i>
+                @endif
+
                 {{ $item['title'] }}
                 @unless(empty($item['count']))
                     <span class="badge badge-primary d-inline-block py-1 px-2 ml-2 badge-pill">
