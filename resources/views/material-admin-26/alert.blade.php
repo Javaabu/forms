d5697cd9e0ee827db321e6cc811edd527f26bca6
athphane <athphane@gmail.com>
2025-07-10 13:14:58 +0500

    feat: add Alert component with customizable properties

---
+++ resources/views/material-admin-26/alert.blade.php
@@ -0,0 +1,34 @@
+@php
+    $classes = 'alert alert-' . $type;
+
+    if ($dismissible) {
+        $classes .= ' alert-dismissible fade show';
+    }
+@endphp
+
+<div {{ $attributes->merge(['class' => $classes]) }} role="alert">
+    @if($icon)
+        <div class="d-flex align-items-start">
+            <i class="{{ $icon }} flex-shrink-0 me-2" aria-hidden="true"></i>
+            <div class="flex-grow-1">
+                @isset($heading)
+                    <h4 class="alert-heading">{{ $heading }}</h4>
+                @endisset
+
+                {!! $slot !!}
+            </div>
+        </div>
+    @else
+        @isset($heading)
+            <h4 class="alert-heading">{{ $heading }}</h4>
+        @endisset
+
+        {!! $slot !!}
+    @endif
+
+    @if($dismissible)
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    @endif
+</div>
